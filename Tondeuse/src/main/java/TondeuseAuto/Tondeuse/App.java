607e3852e302a5b7a18a755a4eeaab3bdd027300
Zerouali <oussamazerouali42@gmail.com>
2019-12-25 13:39:45 +0100

    Création du projet

---
+++ Tondeuse/src/main/java/TondeuseAuto/Tondeuse/App.java
@@ -0,0 +1,13 @@
+package TondeuseAuto.Tondeuse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
