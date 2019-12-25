ee36f51e64fb3dc6bfc892349ffd49d82d518f4d
Zerouali <oussamazerouali42@gmail.com>
2019-12-25 21:32:50 +0100

    To Launch application

--- Tondeuse/src/main/java/TondeuseAuto/Tondeuse/App.java
+++ Tondeuse/src/main/java/TondeuseAuto/Tondeuse/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Tondeuse Application" );
         
         
     	if(args != null && args.length == 1) {
