4bd4b643f4c7a93f3e79105fe716d6e66047fb19
Zerouali <oussamazerouali42@gmail.com>
2019-12-25 21:30:54 +0100

    Gitignore and deleted files

--- Tondeuse/src/main/java/TondeuseAuto/Tondeuse/App.java
+++ Tondeuse/src/main/java/TondeuseAuto/Tondeuse/App.java
@@ -1,13 +1,27 @@
 package TondeuseAuto.Tondeuse;
 
-/**
- * Hello world!
- *
- */
+import TondeuseAuto.Tondeuse.exceptions.FileException;
+import TondeuseAuto.Tondeuse.service.TondeuseConfiguratorByFile;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        
+    	if(args != null && args.length == 1) {
+			try {
+				TondeuseConfiguratorByFile tondeusesConfigurator = new TondeuseConfiguratorByFile(args[0]);
+				System.out.println(tondeusesConfigurator.starTondeuse());
+			} catch (FileException e) {
+				System.out.println(e.getMessageLong());
+			}
+		}
+		else {
+			System.out.println("il faut un nom  valide en argument.");
+		}
+	}
     }
-}
+
